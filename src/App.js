4d1f787131acca9ad5344936f912d94c105cb24e
Ahmad Amin <baigahmad323@gmail.com>
2025-02-25 09:46:41 -0800

    added the test script

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { About, Footer, Header, Skills, Testimonial, Work } from './container';
 import { Navbar } from './components';
 import './App.scss';
 
 const App = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = "https://www.tailorflow.ai/chat-script.js?public_id=L_5ddGSskvDM-AaQcdvqgoy6o_HAHb3q";
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <div className="app">
       <Navbar />
